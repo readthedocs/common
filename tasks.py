f2488c5510c0c40f73776ca9044fa7993ca97b2b
Anthony Johnson <aj@ohess.org>
2018-06-08 13:28:20 -0600

    More docs

--- tasks.py
+++ tasks.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
-"""Read the Docs invoke tasks."""
+"""
+Read the Docs common tasks.
+
+This is used from a repository that uses the common repository. You can see what
+tasks are available with::
+
+    invoke -l
+
+To get more info on tasks::
+
+    invoke -h setup-labels
+"""
 
 from __future__ import division, print_function, unicode_literals
 
@@ -15,6 +26,9 @@ from invoke import task, Exit
 # This is the symlink path in the repo
 REPO_PATH = os.path.dirname(__file__)
 SETUP_SECTION = 'tool:release'
+
+# Label class used below for setup-labels
+Label = namedtuple('Label', ['name', 'color', 'desc', 'transpose'])
 
 
 @task
@@ -143,11 +157,14 @@ def get_config(config=None):
     return release_config
 
 
-Label = namedtuple('Label', ['name', 'color', 'desc', 'transpose'])
-
 @task
 def setup_labels(ctx, repo, dry_run=False):
-    """Setup repository labels"""
+    """Setup shared repository labels
+
+    You can specify ``--dry-run/-d`` in order to avoid making changes
+    immediately. Note that the actual actions will differ on a live run as the
+    list of labels is polled twice.
+    """
     try:
         from github import Github, GithubException
     except ImportError:
@@ -161,6 +178,7 @@ def setup_labels(ctx, repo, dry_run=False):
         )))
         return False
 
+    # Current base for labels across repos
     labels = [
         Label('Accepted', '10ff91', 'Accepted issue on our roadmap', []),
         Label('Bug', 'FF666E', 'A bug', ['bug']),
@@ -193,6 +211,7 @@ def setup_labels(ctx, repo, dry_run=False):
         Label('Support', '5494E8', 'Support question', ['question', 'Question']),
     ]
 
+    # Labels we determined we don't use
     delete_labels = [
         'duplicate',
         'help wanted',
@@ -217,7 +236,6 @@ def setup_labels(ctx, repo, dry_run=False):
     for label in labels:
         found = None
 
-        # Look for exact match first, then transposable labels
         for existing_label in existing_labels:
             if label.name == existing_label.name:
                 found = existing_label
@@ -228,20 +246,22 @@ def setup_labels(ctx, repo, dry_run=False):
                     found = existing_label
                     break
 
-        # We have a label to modify, update the label
+        # Modify or create a new label. We can't detect changes here as
+        # description is never available on the label object
         if found:
-            # Cool, description is never updated. We'll just force the update
             print('Updating label: {0}'.format(found.name))
             if not dry_run:
                 found.edit(label.name, label.color, label.desc)
-        # We have to create the label instead
         else:
             print('Creating label: {0}'.format(label.name))
             if not dry_run:
                 repo.create_label(label.name, label.color, label.desc)
 
-    # Second pass: delete bad labels, look for untransposed labels (manual
-    # invention is required here), and note any foreign labels
+    # Second pass, we:
+    #
+    # * Labels that should be deleted
+    # * Labels that can't be transpose directly, issues need to be moved
+    # * Notes unknown labels
     untouched = set()
     for label in repo.get_labels():
         if label.name in delete_labels:
