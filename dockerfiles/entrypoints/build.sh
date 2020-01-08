c8311a96ba21ed236d80289752ed6fad0a415879
Santos Gallegos <santos_g@outlook.com>
2020-01-08 18:17:18 -0500

    Use double quotes

--- dockerfiles/entrypoints/build.sh
+++ dockerfiles/entrypoints/build.sh
@@ -2,7 +2,7 @@
 
 ../../docker/common.sh
 
-CMD='python3 -m celery worker -A ${CELERY_APP_NAME}.worker -Ofair -c 2 -Q builder,celery,default,build01 -l DEBUG'
+CMD="python3 -m celery worker -A ${CELERY_APP_NAME}.worker -Ofair -c 2 -Q builder,celery,default,build01 -l DEBUG"
 
 if [ -n "${DOCKER_NO_RELOAD}" ]; then
   echo "Running Docker with no reload"
