3e5ffe3ff631adbfa26ea09a7880d52fdd77a8db
Manuel Kaufmann <humitos@gmail.com>
2020-03-16 09:05:14 -0700

    Add CORS headers to storage

--- dockerfiles/scripts/create-containers.sh
+++ dockerfiles/scripts/create-containers.sh
@@ -6,4 +6,6 @@ then
     az storage container create --connection-string $CONNECTION_STRING --public-access "blob" --name "builds"
     az storage container create --connection-string $CONNECTION_STRING --public-access "blob" --name "media"
     az storage container create --connection-string $CONNECTION_STRING --public-access "blob" --name "static"
+
+    az storage cors add --connection-string $CONNECTION_STRING --origins 'community.dev.readthedocs.io' --services 'b' --methods GET OPTIONS HEAD --allowed-headers '*'
 fi
