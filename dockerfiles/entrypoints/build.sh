a78d01b21195012c767521b168765f43d1da5ff2
Manuel Kaufmann <humitos@gmail.com>
2020-06-08 12:31:28 +0200

    Use `docker-compose --scale build=N` to manually set N builders

--- dockerfiles/entrypoints/build.sh
+++ dockerfiles/entrypoints/build.sh
@@ -2,7 +2,7 @@
 
 ../../docker/common.sh
 
-CMD="python3 -m celery worker -A ${CELERY_APP_NAME}.worker -Ofair -c 1 -Q builder,celery,default,build01,{CELERY_QUEUE} -l DEBUG"
+CMD="python3 -m celery worker -A ${CELERY_APP_NAME}.worker -Ofair -c 1 -Q builder,celery,default,build01,build:default,build:large -l DEBUG"
 
 if [ -n "${DOCKER_NO_RELOAD}" ]; then
   echo "Running Docker with no reload"
