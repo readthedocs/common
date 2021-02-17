b91d73acea372c75cabd48b22601294d0dcb1103
Manuel Kaufmann <humitos@gmail.com>
2021-02-17 18:55:34 +0100

    Create `test` user on init
    This user is required with the current settings because it's used by builders as
    SLUMBER user to hit APIv2 and send commands, request versions, etc.

--- dockerfiles/scripts/createsuperuser.py
+++ dockerfiles/scripts/createsuperuser.py
@@ -2,3 +2,6 @@ from django.contrib.auth.models import User
 
 if not User.objects.filter(username='admin').exists():
     User.objects.create_superuser('admin', 'admin@admin.com', 'admin')
+
+if not User.objects.filter(username='test').exists():
+    User.objects.create_superuser('test', 'test@test.com', 'test')
