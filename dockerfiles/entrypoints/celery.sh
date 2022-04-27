2cb07a517b11f987ae3611fe2ba6ac903f8830db
Santos Gallegos <stsewd@protonmail.com>
2022-04-27 14:19:47 -0500

    Make patterns more specific for watchmedo

--- dockerfiles/entrypoints/celery.sh
+++ dockerfiles/entrypoints/celery.sh
@@ -12,7 +12,7 @@ if [ -n "${DOCKER_NO_RELOAD}" ]; then
 else
   echo "Running Docker with reload"
   watchmedo auto-restart \
-  --patterns="*.py" \
+  --patterns="./readthedocs/*.py;./readthedocsinc/*.py" \
   --ignore-patterns="*.#*.py;*.pyo;*.pyc;*flycheck*.py;*test*;*migrations*;*management/commands*" \
   --ignore-directories \
   --recursive \
