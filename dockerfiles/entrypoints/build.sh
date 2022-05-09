26affe7c37143d3ef8f6ce15462db5c7413280f4
Santos Gallegos <stsewd@protonmail.com>
2022-05-09 12:39:28 -0500

    Celery: keep cancelled builds cancelled (#109)
    When a task is revoked, it still needs to be picked up by a worker to "delete it",
    in the meantime celery will keep those revoked tasks in memory,
    if you restart celery, that list is lost, and the tasks will be executed after a worker picks it up.
    
    Ref https://docs.celeryq.dev/en/stable/userguide/workers.html#persistent-revokes

--- dockerfiles/entrypoints/build.sh
+++ dockerfiles/entrypoints/build.sh
@@ -2,7 +2,14 @@
 
 ../../docker/common.sh
 
-CMD="python3 -m celery -A ${CELERY_APP_NAME}.worker worker -Ofair -c 1 -Q builder,celery,default,build01,build:default,build:large -l DEBUG"
+# When a task is revoked, it still needs to be picked up by a worker to "delete it",
+# in the meantime celery will keep those revoked tasks in memory,
+# if you restart celery, that list is lost, and the tasks will be executed after a worker picks it up.
+# We need to save that state in a file so isn't lost.
+# Ref https://docs.celeryq.dev/en/stable/userguide/workers.html#persistent-revokes.
+CELERY_STATE_DIR=/var/run/celery/
+mkdir -p $CELERY_STATE_DIR
+CMD="python3 -m celery -A ${CELERY_APP_NAME}.worker worker -Ofair -c 1 -Q builder,celery,default,build01,build:default,build:large -l DEBUG --statedb=${CELERY_STATE_DIR}worker.state"
 
 if [ -n "${DOCKER_NO_RELOAD}" ]; then
   echo "Running Docker with no reload"
