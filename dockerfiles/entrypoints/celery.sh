ef7e209fc415b53e0749757231e0642a7ed81506
Benjamin Bach <benjamin@overtag.dk>
2022-06-15 17:14:22 +0200

    Reduces timeout, removes a dangling comment

--- dockerfiles/entrypoints/celery.sh
+++ dockerfiles/entrypoints/celery.sh
@@ -5,7 +5,7 @@
 # If SEARCH is enabled, the celery container should wait for the search
 # container to be ready
 if [ "$SEARCH" != "" ]; then
-  ../../docker/wait-for-it.sh search:9200 --timeout=360 --strict
+  ../../docker/wait-for-it.sh search:9200 --timeout=180 --strict
 fi
 
 CMD="python3 -m celery -A ${CELERY_APP_NAME}.worker worker -Ofair -c 2 -Q web,web01,reindex,autoscaling -l DEBUG"
