ac1215ef07145d5c5fe21d521d30bf1a5d0f178b
Anthony <aj@ohess.org>
2023-02-02 10:16:56 -0800

    Use nodemon for reloading on all server processes (#160)
    * Use nodemon for reloading on all server processes
    
    This examples nodemon from the celery entrypoint script to the web and
    proxito service entrypoint scripts.
    
    The Django runserver --reload option doesn't see to be configurable, and
    seems to watch a lot of files that are not needed for reloading. I can't
    tell what exactly is being watched by runserver, but I am getting
    frequent `too many open files` errors from processes in Docker. Running with
    --no-reload mostly solves this issue, but requires manually restarting
    services.
    
    Someone else should spend some time with this, I'm not confident that this will:
    
    1. Fix the issue for others
    2. Not cause irreparable damage to your local installation
    
    * Actually noreload on django runserver commands
    
    * Use gunicorn
    
    * Update nodemon configuration
    
    * More nodemon settings
    
    ---------
    
    Co-authored-by: Manuel Kaufmann <humitos@gmail.com>

--- dockerfiles/entrypoints/proxito.sh
+++ dockerfiles/entrypoints/proxito.sh
@@ -2,13 +2,12 @@
 
 ../../docker/common.sh
 
-if [ -n "${DOCKER_NO_RELOAD}" ];
-then
-    RELOAD='--noreload'
-    echo "Running Docker with no reload"
-else
-    RELOAD=''
-    echo "Running Docker with reload"
-fi
+CMD="gunicorn readthedocs.wsgi:application -w 3 -b 0.0.0.0:8000 --max-requests=10000"
 
-python3 manage.py runserver 0.0.0.0:8000 $RELOAD
+if [ -n "${DOCKER_NO_RELOAD}" ]; then
+  echo "Running process with no reload"
+  $CMD
+else
+  echo "Running process with reload"
+  nodemon --config ../nodemon.json --exec "${CMD}"
+fi
