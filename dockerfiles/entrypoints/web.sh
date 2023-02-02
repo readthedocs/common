2ef4833b74661c4f47e3201925f44a7a9579e334
Manuel Kaufmann <humitos@gmail.com>
2023-02-02 17:27:17 +0100

    More nodemon settings

--- dockerfiles/entrypoints/web.sh
+++ dockerfiles/entrypoints/web.sh
@@ -13,12 +13,12 @@ then
     python3 manage.py loaddata test_data
 fi
 
-CMD="gunicorn readthedocs.wsgi:application -w 2 -b 0.0.0.0:8000 --max-requests=10000"
+CMD="gunicorn readthedocs.wsgi:application -w 3 -b 0.0.0.0:8000 --max-requests=10000"
 
 if [ -n "${DOCKER_NO_RELOAD}" ]; then
   echo "Running process with no reload"
   $CMD
 else
   echo "Running process with reload"
-  nodemon --config ../nodemon.json --exec $CMD
+  nodemon --config ../nodemon.json --exec "${CMD}"
 fi
