6f485b0b083518349c15330310a23fc6418e180b
Manuel Kaufmann <humitos@gmail.com>
2023-04-19 17:15:47 +0200

    Celery beat: remove .db at startup to avoid corrupted db (#173)
    * Celery beat: remove .db at startup to avoid corrupted db
    
    * Update dockerfiles/entrypoints/celery-beat.sh

--- dockerfiles/entrypoints/celery-beat.sh
+++ dockerfiles/entrypoints/celery-beat.sh
@@ -4,6 +4,11 @@
 
 CMD="python3 -m celery -A ${CELERY_APP_NAME}.worker beat -l ${CELERY_LOG_LEVEL}"
 
+# Not sure why, but the db becomes corrupted at some point
+# and make the startup of this process to fail.
+# Removing it makes Celery to re-create it.
+rm -f celerybeat-schedule.db
+
 if [ -n "${DOCKER_NO_RELOAD}" ]; then
   echo "Running process with no reload"
   $CMD
