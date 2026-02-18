434095d2c335746e705030d35d2ee6cd297a5903
Manuel Kaufmann <humitos@gmail.com>
2026-02-18 16:34:53 +0100

    Use RustFS instead of MinIO for storage
    Closes https://github.com/readthedocs/readthedocs.org/issues/12790

--- dockerfiles/entrypoints/createbuckets.sh
+++ dockerfiles/entrypoints/createbuckets.sh
@@ -4,16 +4,16 @@
 test "${INIT}" || exit 0
 
 /usr/src/app/docker/wait-for-it.sh storage:9000 --timeout=30 --strict
-/usr/bin/mc alias set myminio http://storage:9000 "$MINIO_ROOT_USER" "$MINIO_ROOT_PASSWORD";
-/usr/bin/mc mb myminio/static;
-/usr/bin/mc policy set public myminio/static;
-/usr/bin/mc mb myminio/media;
-/usr/bin/mc policy set public myminio/media;
-/usr/bin/mc mb myminio/build-tools;
-/usr/bin/mc policy set public myminio/build-tools;
-/usr/bin/mc mb myminio/builds;
-/usr/bin/mc policy set public myminio/builds;
-/usr/bin/mc mb myminio/usercontent;
-/usr/bin/mc policy set public myminio/usercontent;
+/usr/bin/mc alias set myrustfs http://storage:9000 "$RUSTFS_ROOT_USER" "$RUSTFS_ROOT_PASSWORD";
+/usr/bin/mc mb myrustfs/static;
+/usr/bin/mc policy set public myrustfs/static;
+/usr/bin/mc mb myrustfs/media;
+/usr/bin/mc policy set public myrustfs/media;
+/usr/bin/mc mb myrustfs/build-tools;
+/usr/bin/mc policy set public myrustfs/build-tools;
+/usr/bin/mc mb myrustfs/builds;
+/usr/bin/mc policy set public myrustfs/builds;
+/usr/bin/mc mb myrustfs/usercontent;
+/usr/bin/mc policy set public myrustfs/usercontent;
 exit 0;
 
