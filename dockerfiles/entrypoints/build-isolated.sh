c1cd7d2d109aa5540e119d58f0ebcdc5ee31f852
Manuel Kaufmann <humitos@gmail.com>
2026-09-09 22:25:50 +0200

    Celery: start without mingle (match production)
    See https://github.com/readthedocs/readthedocs-ops/pull/1850

--- dockerfiles/entrypoints/build-isolated.sh
+++ dockerfiles/entrypoints/build-isolated.sh
@@ -56,7 +56,7 @@ UV_PROJECT_ENVIRONMENT="$VENV" \
 echo "Starting Celery worker on queue '$RTD_BUILDS_QUEUE' ..."
 export PYTHONPATH="$SRC/worker"
 
-CMD="$VENV/bin/celery -A worker.celery worker --loglevel=INFO --concurrency=1 --max-tasks-per-child=1 -Q ${RTD_BUILDS_QUEUE}"
+CMD="$VENV/bin/celery -A worker.celery worker --loglevel=INFO --concurrency=1 --max-tasks-per-child=1 --without-mingle --without-gossip --without-heartbeat -Q ${RTD_BUILDS_QUEUE}"
 if [ -n "${DOCKER_NO_RELOAD}" ]; then
   echo "Running process with no reload"
   exec $CMD
